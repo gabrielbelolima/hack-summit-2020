cc39d879cb624141bdba82baa92dea2ad0ff2f22
psilva009 <marips0202@gmail.com>
2020-08-23 09:07:05 -0300

    Making the page static

--- layout/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer">
-<p>B2W - Companhia Digital / CNPJ: 00.776.574/0006-60 / Inscrição Estadual: 85.687.08-5 / Endereço Rua Sacadura Cabral, 102 - Rio de Janeiro, RJ - 20081-902 / <a href="https://www.americanas.com.br/central-de-atendimento">https://www.americanas.com.br/central-de-atendimento</a><p>
-</div>
